77ad16317ca52999a0b64337a11053d00458ea32
Jim O'Connell <jim@mmdc.net>
2016-06-25 17:40:26 -0400

    tweak

--- led.py
+++ led.py
@@ -11,8 +11,8 @@ GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM
 GPIO.setup(25, GPIO.OUT)# set GPIO 25 as output for white led  
 GPIO.setup(24, GPIO.OUT)# set GPIO 24 as output for red led  
   
-white = GPIO.PWM(24, 100)    # create object white for PWM on port 25 at 100 Hertz  
-red = GPIO.PWM(25, 100)      # create object red for PWM on port 24 at 100 Hertz  
+white = GPIO.PWM(25, 100)    # create object white for PWM on port 25 at 100 Hertz  
+red = GPIO.PWM(24, 100)      # create object red for PWM on port 24 at 100 Hertz  
   
 white.start(0)              # start white led on 0 percent duty cycle (off)  
 red.start(100)              # red fully on (100%)  
@@ -20,11 +20,11 @@ red.start(100)              # red fully on (100%)
 # now the fun starts, we'll vary the duty cycle to   
 # dim/brighten the leds, so one is bright while the other is dim  
   
-pause_time = 0.04           # you can change this to slow down/speed up  
+pause_time = 0.02           # you can change this to slow down/speed up  
   
 try:  
     while True:  
-        for i in range(30,101):      # 101 because it stops when it finishes 100  
+        for i in range(50,101):      # 101 because it stops when it finishes 100  
             white.ChangeDutyCycle(i)  
             red.ChangeDutyCycle(100 - i)  
             sleep(pause_time)  
