d4844472069536123d4e5e2eb80195b44d876fdd
Jim O'Connell <jim@mmdc.net>
2016-06-25 20:03:30 -0400

    tweak

--- led.py
+++ led.py
@@ -20,15 +20,15 @@ red.start(100)              # red fully on (100%)
 # now the fun starts, we'll vary the duty cycle to 
 # dim/brighten the leds, so one is bright while the other is dim
 
-pause_time = 0.02           # you can change this to slow down/speed up
+pause_time = 0.04           # you can change this to slow down/speed up
 
 try:
     while True:
-        for i in range(0,101):      # 101 because it stops when it finishes 100
+        for i in range(15,101,5):      # 101 because it stops when it finishes 100
             white.ChangeDutyCycle(i)
             red.ChangeDutyCycle(100 - i)
             sleep(pause_time)
-        for i in range(100,-1,-1):      # from 100 to zero in steps of -1
+        for i in range(100,-1,-5):      # from 100 to zero in steps of -1
             white.ChangeDutyCycle(i)
             red.ChangeDutyCycle(100 - i)
             sleep(pause_time)
