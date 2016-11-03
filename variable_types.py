c7c7aab8b06247a711f8d3cb8b6255f3bda47be1
DJ Troutman <bakerlady622@yahoo.com>
2016-11-03 15:10:57 -0400

    adding dj's lesson on types

---
+++ variable_types.py
@@ -0,0 +1,31 @@
+number = 9
+print(type(number))   # print type of variable "number"
+
+float_number = 9.0
+print (type(float_number))
+
+
+foo = "hi there"
+foo = 1000000
+#print (type(foo))
+
+shopping = ["Ham", "Eggs", "bread", "dogfood"]
+print (type(shopping))
+
+print(shopping[0])
+
+things = [ shopping, 'chemistry', 1997, 2000];
+
+
+####################################
+things[0][3] = "CatFood"
+print(things[0][3])
+print (shopping[3])
+print(type(shopping[3]))
+####################################
+
+foo = "This is the new foo"
+
+print(things[0])
+
+print(foo)
