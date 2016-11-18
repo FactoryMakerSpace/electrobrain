e55d8eb774a11f669a6134abc06f90983e5af58d
jimoconnell <jim@mmdc.net>
2016-11-18 10:57:01 -0500

    Adding "while.py" to "Conditionals"

---
+++ conditionals/while.py
@@ -0,0 +1,16 @@
+#!/usr/bib/python3
+
+# Another type of conditional is "While"
+# Read the following code aloud and see if you can understand it:
+# (Remember that "<" is 'Less Than"!)
+
+foo = 1
+
+while (foo < 100):
+    print ("Foo is less than 100, it's only %d" % foo)
+    foo = foo + 1
+
+print("Foo is now %d!" % foo)
+
+# ToDo:
+# Write some Python code below where foo starts at 100 and counts down to zero
